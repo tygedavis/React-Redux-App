458f90fd679149fa7e81d66cfc2f4c1fc27c40b9
tygedavis <tyge.davis@gmail.com>
2019-12-12 16:38:08 -0700

    App styled

--- src/App.js
+++ src/App.js
@@ -4,25 +4,34 @@ import { connect } from "react-redux";
 import { getQuote } from './actions/index';
 
 import Loader from 'react-loader-spinner';
+import KanyeWest from './img/KanyeWest.jpg'
 import './App.css';
 
 function App(props) {
   console.log('Props on App: ', props)
   return (
     <div className="App">
-      <h1>Kanyes Kuotes</h1>
-      <p>You're daily dose of Kanye inspiration</p>
-      <button onClick={props.getQuote}>Klick for Kuotes</button>
-      {!props.quote && !props.isFetching && (<p>Kanye Goes Here</p>)}
-      {props.isFetching && (
-        <Loader
-          type="ThreeDots"
-          color="#00BFFF"
-          height={100}
-          width={100}
-          />
-      )}
-      {props.quote && (<p>"{props.quote}" -Kanye West</p>)}
+      <div className="body">
+        <div className="header">
+          <h1>Kanyes Kuotes</h1>
+          <p>You're daily dose of Kanye inspiration</p>
+        </div>
+        <button onClick={props.getQuote}>Klick for Kuotes</button>
+        {!props.quote && !props.isFetching && (<img src={KanyeWest} alt='Kanye West' className='kanye'/>)}
+        {props.isFetching && (
+          <Loader
+            type="ThreeDots"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            />
+        )}
+        
+        {props.quote && (
+          <div className='quote'>
+            <p>"{props.quote}" -Kanye West</p>
+          </div>)}
+      </div>
     </div>
   );
 }
