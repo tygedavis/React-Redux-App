d658b832b7677f71690a783eda9522dae45ad8a4
tygedavis <tyge.davis@gmail.com>
2019-12-12 15:46:37 -0700

    Quotes render on the screen when button is clicked

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { connect } from "react-redux";
 
+import { getQuote } from './actions/index';
+
+import Loader from 'react-loader-spinner';
 import './App.css';
 
-function App() {
+function App(props) {
+  console.log('Props on App: ', props)
   return (
     <div className="App">
       <h1>Kanyes Kuotes</h1>
       <p>You're daily dose of Kanye inspiration</p>
-      <button>Klick for Kuotes</button>
+      <button onClick={props.getQuote}>Klick for Kuotes</button>
+      {!props.quote && !props.isFetching && (<p>Kanye Goes Here</p>)}
+      {props.isFetching && (
+        <Loader
+          type="ThreeDots"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          />
+      )}
+      {props.quote && (<p>"{props.quote}" -Kanye West</p>)}
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    quote: state.quote,
+    isFetching: state.isFetching,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps, { getQuote })(App);
